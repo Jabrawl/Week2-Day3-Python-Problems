932a7d54131c36f25d0b415c49607632cb90b3ff
Jabrawl <jamesstibral@gmail.com>
2023-01-06 09:21:14 -0600

    Day 3 Homework

--- calculator.py
+++ calculator.py
@@ -1,9 +1,16 @@
+import math
 
-from math import pi
+def squareFootage():
+    length = int(input("Enter the length of the house (in feet): "))
+    width = int(input("Enter the width of the house (in feet): "))
+    area = length * width
+    print(f"The area of your house is {area} Ft")
 
-def squareFootage(length, width):
-    return length * width
+
+def circumCircle():
+    radius = int(input("Enter the radius of your circle (in inches): "))
+    circum = radius * math.pi * 2
+    print(f"The Circumference of your circle is {math.ceil(circum)} inches long")
 
 
-def circumCircle(radius):
-    return round(radius * 2 * pi, 2)
+
